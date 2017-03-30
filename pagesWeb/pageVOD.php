d89b5e3ac0396d55f5e7e1a6a4c762ad463d68bd
amam37 <elhadjiamadou.gueye@gmail.com>
2017-03-30 19:24:09 +0000

    Add files via upload

--- pagesWeb/pageVOD.php
+++ pagesWeb/pageVOD.php
@@ -1,11 +1,71 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>FREE RESPONSIVE HORIZONTAL ADMIN</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
 
+    <!-- CUSTOM STYLE  -->
+    <link href="style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
-<?php
+</head>
+<body>
+    <div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
 
-$video = "http://localhost:8080/Ressources/Videos/"."$_GET['nom']";
+                    PLATEFORME DE VIDEOS EDUCATIVES
+                </a>
 
+            </div>
 
-print    "<video controls autoplay name='media'>
-          <source type='video/mp4' src='$video'>
-            Ici la description alternative
-        </video>";
+        </div>
+    </div>
+     <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line"><?php echo $_GET['nom'];?></h4>
+                
+                            </div>
+
+        </div>
+             
+             <div class="row">
+
+              <div class="col-md-8 col-sm-8 col-xs-12">
+              <?php
+                    $video = "http://localhost/Projet_Prog_linux-master/Ressources/Videos/".$_GET['nom'];
+
+                    print   "<video controls autoplay name='media'>
+                     <source type='video/mp4' src='$video'>
+                                Ici la description alternative
+                 </video>" ;
+
+            ?>
+                 
+                        
+
+                 </div>
+                
+            
+</body>
+
+</html>>
